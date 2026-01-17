d0d66fb4ae200003b201c1002ae3d4d7e44c1c66
lparlett <93846508+lparlett@users.noreply.github.com>
2026-01-17 18:24:32 -0500

    fix: Update import statement for static_resources in update_stylesheets script
    Signed-off-by: lparlett <93846508+lparlett@users.noreply.github.com>

--- .github/scripts/update_stylesheets.py
+++ .github/scripts/update_stylesheets.py
@@ -1,5 +1,5 @@
 """Update CDA stylesheets from HL7 repository."""
 
-from health_records_collection.frontend import static_resources
+from frontend import static_resources
 
 static_resources.update_static_files(force=True)
